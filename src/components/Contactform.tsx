c3619b9ad9bb10c338b66fd75fbe0fa31ad16268
Henry Loveday <henrygad.orji@gmail.com>
2025-07-21 12:24:03 +0100

    changes on env

--- src/components/Contactform.tsx
+++ src/components/Contactform.tsx
@@ -5,9 +5,9 @@ import type { contactFormProps } from "../types/contactForm.type";
 import { type UseFormSetValue } from "react-hook-form";
 import emailjs, { EmailJSResponseStatus } from "@emailjs/browser";
 
-const SERVICE_ID = import.meta.env.SERVICE_ID;
-const TEMPLATE_ID = import.meta.env.TEMPLATE_ID;
-const PUBLIC_KEY = import.meta.env.PUBLIC_KEY;
+const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
 
 const Contactform = () => {
   const [isLoading, setIsLoading] = useState(false);
